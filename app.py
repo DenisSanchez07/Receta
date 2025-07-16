d072e5571f0e6a70a448512990ca517a63d6e125
DenisSanchez07 <166968038+DenisSanchez07@users.noreply.github.com>
2025-07-15 19:32:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def main():
     unidades = df["Unidad"].dropna().unique()
     unidad_seleccionada = st.selectbox("Selecciona la unidad:", unidades)
 
-    productos = df[df["Unidad"] == unidad_seleccionada]["Codigo"].unique()
+    productos = df[df["Unidad"] == unidad_seleccionada]["Tipo_Producto"].unique()
     producto_seleccionado = st.selectbox("Selecciona el producto químico:", productos)
 
     data = df[(df["Unidad"] == unidad_seleccionada) & (df["Producto químico"] == producto_seleccionado)].iloc[0]
