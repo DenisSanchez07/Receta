e9e9b0e2ee3aa9d36ff494a3e53df5942fc27adb
DenisSanchez07 <166968038+DenisSanchez07@users.noreply.github.com>
2025-07-15 20:26:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ st.title("🧪 Preparación de Productos Químicos por Unidad")
 # Cargar datos desde el archivo Excel
 @st.cache_data
 def cargar_datos():
+    df.columns = df.columns.str.strip()
     df = pd.read_excel("plantilla_recetas_productos.xlsx", sheet_name="Sheet1")
     return df
 
